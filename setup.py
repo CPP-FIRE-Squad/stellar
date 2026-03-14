14b0744b6b643069ae1a6c5c0a0375287ee627d5
somethingstamo <insertuniqueemail@gmail.com>
2026-03-13 19:45:01 -0700

    Test fix for setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     url='https://github.com/CPP-FIRE-Squad/stellar',
     py_modules=['Halo', 'Simulation', 'Particle'],
     install_requires=[
-        'astropy', "git+https://bitbucket.org/awetzel/gizmo_analysis.git"
+        'astropy', "gizmo_analysis @ git+https://bitbucket.org/awetzel/gizmo_analysis.git"
     ],
 )
