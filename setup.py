eb87edb80b279e301b925335104b49edc3464d7a
olive <isnertuniqueemail@gmail.com>
2026-03-13 18:57:16 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='stellar',
+    version='2.0.0',
+    author='Olive Stam, Cameron Ross, Austin Martinez',
+    author_email='osstam@cpp.edu, ceross@cpp.edu, austinm@cpp.edu',
+    description= 'A utility package for CPP Fire Squad',
+    license='MIT',
+    url='https://github.com/CPP-FIRE-Squad/stellar',
+    py_modules=['stellar'],
+    install_requires=[
+        'astropy'
+    ],
+)
